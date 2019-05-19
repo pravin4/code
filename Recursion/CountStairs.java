05748b7b3c5086e8509e154ac40ad72193a8c164
prajadhav <Pravin.Jadhav@ebay.com>
2019-05-18 18:08:32 -0700

    next

--- Recursion/CountStairs.java
+++ Recursion/CountStairs.java
@@ -3,15 +3,17 @@ package Recursion;
 public class CountStairs {
 	
 	public static void main(String[] args) {
-		int numStairs = 3;
-		int[] step = {1,2};
+		int numStairs = 6;
+		int[] step = {2,3};
 		System.out.println(countStairs(numStairs, step));
+		System.out.println(countStairsDP(numStairs, step));
+
 	}
 	
 	static int countStairs(int numStairs, int[] step) {
 		
 		//recurrence base case
-		if (numStairs ==0) return 1;
+		if (numStairs == 0) return 1;
 		if (numStairs < 0) return 0;
 		
 		//recurrence relationship: f(n) = Sum from (i to k) {f(n - step[i])}
@@ -20,5 +22,24 @@ public class CountStairs {
 			result += countStairs(numStairs-step[i], step);
 		}
 		return result;
+	}
+	
+	
+	static int countStairsDP(int numStairs, int[] step) {
+		
+		int[] currentCount = new int[numStairs+1];
+		
+		currentCount[0] = 1;
+		
+		for (int i=1; i<=numStairs; i++) {
+			for (int currentStep: step) {
+				int previousStep = i-currentStep;
+				if (previousStep >= 0) {
+					currentCount[i] += currentCount[previousStep];
+				}
+			}
+		}
+		
+		return currentCount[numStairs];
 	}
 }
